2bacd2eccc9ac3f6d9ea99f0a256cf5dcbb922ee
maiquel <maiquel_craash@hotmail.com>
2016-11-16 23:12:57 -0200

    SlideShow funcional lendo a partir de JSON com Title e Caption, OK!

--- Client/main.js
+++ Client/main.js
@@ -8,26 +8,26 @@ window.onload = function () {
 	 "http://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/dada-voltaire-tinkerbot.jpg",
 	 "http://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/happy-bot-tinkerbot.jpg",
 	 "https://pbs.twimg.com/profile_images/583194780039045121/LF6M0JEu.jpg"];*/
-	
+
 	var slides = [];
-	
+
 
 	/* JSON que vem do Server */
 	var remote_images = {
 		images: [
 			{
 				url: "https://pbs.twimg.com/profile_images/583194780039045121/LF6M0JEu.jpg",
-				title:"Cachorro Quente",
+				title: "Cachorro Quente",
 				caption: "Comendo"
 			},
 			{
 				url: "http://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/happy-bot-tinkerbot.jpg",
-				title:"Robô",
+				title: "Robô",
 				caption: "Feliz"
 			},
 			{
 				url: "http://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/dada-voltaire-tinkerbot.jpg",
-				title:"Outro robo",
+				title: "Outro robo",
 				caption: "Triste pq n demos nome"
 			}
 		]
@@ -45,13 +45,17 @@ window.onload = function () {
 		var slide = new Slide(element.url, element.title, element.caption);
 		slides.push(slide);
 	});
-	
-	
-	
+
+
 	var slideshow = document.getElementById("slideshow");
 
 	slides.forEach(function (slide) {
-		var img = "<img src='" + slide.url + "'>";
+		var img =
+			"<div class='slide'>" +
+			"	<div class='title'>" + slide.title + "</div>" +
+			"	<img src='" + slide.url + "'>" +
+			"	<div class='caption'>" + slide.caption + "</div>" +
+			"</div>";
 		slideshow.insertAdjacentHTML("beforeend", img);
 	});
 
@@ -72,20 +76,20 @@ window.onload = function () {
 	/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
 
 
-	var imgs = document.getElementById('slideshow').children;					// pega todos os imgs
-	var interval = 8000;														// duração do efeito
-	var currentPic = 0;
-	imgs[currentPic].style.webkitAnimation = 'fadey ' + interval + 'ms';		// anima a primeira imagem
-	imgs[currentPic].style.animation = 'fadey ' + interval + 'ms';
-	var infiniteLoop = setInterval(function () {								// loo infinito
-		imgs[currentPic].removeAttribute('style');								// remove o efeito da imagem atual
-		if (currentPic == imgs.length - 1) {									// se foi a última, volta pra primeira
-			currentPic = 0;
+	var slides = document.getElementById('slideshow').children;						// pega todos os imgs
+	var interval = 8000;															// duração do efeito
+	var currentSlide = 0;
+	slides[currentSlide].style.webkitAnimation = 'fadey ' + interval + 'ms';		// anima a primeira imagem
+	slides[currentSlide].style.animation = 'fadey ' + interval + 'ms';
+	var infiniteLoop = setInterval(function () {									// loo infinito
+		slides[currentSlide].removeAttribute('style');								// remove o efeito da imagem atual
+		if (currentSlide == slides.length - 1) {									// se foi a última, volta pra primeira
+			currentSlide = 0;
 		} else {
-			currentPic++;														// se não, avança a imagem
+			currentSlide++;															// se não, avança a imagem
 		}
-		imgs[currentPic].style.webkitAnimation = 'fadey ' + interval + 'ms';    // adiciona efeito de animação na imagem atual
-		imgs[currentPic].style.animation = 'fadey ' + interval + 'ms';
+		slides[currentSlide].style.webkitAnimation = 'fadey ' + interval + 'ms';    // adiciona efeito de animação na imagem atual
+		slides[currentSlide].style.animation = 'fadey ' + interval + 'ms';
 	}, interval);
 
 
