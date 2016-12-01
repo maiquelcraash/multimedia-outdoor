c8354e669a0090010909a323ccabd214416e311c
maiquel <maiquel_craash@hotmail.com>
2016-11-30 22:05:36 -0200

    Efetuados ajustes de layout no client. Agora a velocidade da mensagem é calculada dinamicamente com base no número de caracteres da mensagem.

--- Client/main.js
+++ Client/main.js
@@ -8,23 +8,23 @@ window.onload = function () {
 	var host = "http://localhost:3001/";
 	var remote_images = [];
 
-	//var connection = new WebSocket(host, "standard");
-
 	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.open("GET", host, true); // false for synchronous request
 
 
 	xmlHttp.onload = function (data) {
-		console.log(xmlHttp.response);
+		// console.log(xmlHttp.response);
 		remote_images = JSON.parse(xmlHttp.responseText);
-		console.log(remote_images);
-		render();
+		// console.log(remote_images);
+		renderSlides();
 	};
 
 	xmlHttp.send();
 
+	getWather();
 
-	function render() {
+
+	function renderSlides() {
 		var slides = [];
 
 		/* Objeto Slide */
@@ -35,16 +35,15 @@ window.onload = function () {
 			this.color = color;
 		}
 
-		/* Faz parse do JSON */
+		/* Faz parse do JSON para objeto Slide */
 		remote_images.images.forEach(function (element) {
 			var slide = new Slide(element.url, element.title, element.caption, element.color);
 			console.log(element.color);
 			slides.push(slide);
 		});
 
-
+		/* Cria os componentes HTML para os slides */
 		var slideshow = document.getElementById("slideshow");
-
 		slides.forEach(function (slide) {
 			var img =
 				"<div class='slide'>" +
@@ -55,11 +54,12 @@ window.onload = function () {
 			slideshow.insertAdjacentHTML("beforeend", img);
 		});
 
+		/* Configura mensagens no elemento e define a velocidade de execução de acordo com a quantidade de caracteres a uma taxa de 30 caracteres por segundo*/
 		var remote_message = remote_images.messages.join("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-----&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-
-
 		var message = document.getElementById("message");
 		message.innerHTML = remote_message;
+		var duration = remote_message.length / 20;
+		message.style.animation = "marquee " + duration + "s linear infinite";
 
 
 		/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
@@ -80,9 +80,30 @@ window.onload = function () {
 			slides[currentSlide].style.webkitAnimation = 'fadey ' + interval + 'ms';    // adiciona efeito de animação na imagem atual
 			slides[currentSlide].style.animation = 'fadey ' + interval + 'ms';
 		}, interval);
-
-
 	}
+
+	function getWather(cityCode) {
+		// var request = new XMLHttpRequest();
+		//
+		// request.open('GET', 'http://www.climatempo.com.br/previsao-do-tempo/cidade/1399', true);
+		// request.setRequestHeader("Access-Control-Allow-Origin","*");
+		// request.send();
+		//
+		// request.onload = function (source) {
+		// 	// console.log(request.response);
+		// 	console.log(source);
+		// };
+
+		// var x = document.getElementById("tempo");
+		// var y = (x.contentWindow || x.contentDocument);
+		// if (y.document) y = y.document;
+		//
+		// console.log(y);
+		//
+		// var weatherBox = y.getElementsByClassName("bg-box-prev-topo")[0];
+		// y.body.innerHTML = weatherBox.innerHTML;
+	}
+
 };
 
 
